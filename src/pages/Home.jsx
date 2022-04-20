7d840d11acf87ce5682416226e243c0c4a2fcac7
ELDEVODE01~ <elpraise20@gmail.com>
2022-04-20 12:57:03 +0100

    Initaited hooks

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import UserResults from '../components/users/UserResults';
+function Home() {
+  return (
+    <>
+      {/* Search Component */}
+      <UserResults />
+    </>
+  );
+}
+export default Home;
